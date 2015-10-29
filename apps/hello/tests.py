749aa80cc47bf05caee00d52ac4197ec27ec74be
Vova <vov.m.test@gmail.com>
2015-10-29 18:04:43 +0200

    1st ticket, 2nd commit

--- apps/hello/tests.py
+++ apps/hello/tests.py
@@ -1,9 +1,39 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.core.urlresolvers import reverse
+from django.http import HttpRequest
 
-# Create your tests here.
+from .models import About
+from .views import all_people
 
 
-class SomeTests(TestCase):
-    def test_math(self):
-        "put docstrings in your tests"
-        assert(2 + 2 == 5)
+class PersonTest(TestCase):
+    """ Unit tests for About model and views """
+    fixtures = ['initial_data.json']
+    
+    def create_people(self, name="A", last_name="S", email="q@q.ua", jabber="-", skype="-"):
+        return About.objects.create(name=name,
+                                    last_name=last_name,
+                                    email=email,
+                                    jabber=jabber,
+                                    skype=skype)
+
+    def test_people_creation(self):
+        a = self.create_people()
+        self.assertTrue(isinstance(a, About))
+        self.assertEqual(a.__str__(), a.last_name)
+
+    def test_home_page_available(self):
+        c = Client()
+        response = c.get('/')
+        self.assertEquals(response.status_code, 200)
+
+    def test_home_page_contains_info(self):
+        request = HttpRequest()
+        response = all_people(request)
+        self.assertContains(response, 'Volodymyr')
+        self.assertTrue('<h2>42 Coffee Cups Test Assignment</h2>' in response.content)
+        self.assertContains(response, '<a href="/request/">Request</a>')
+
+    def test_home_page_use_about_template(self):
+        response = self.client.get(reverse('about'))
+        self.assertTemplateUsed(response, 'hello/about.html')
--- fortytwo_test_task/urls.py
+++ fortytwo_test_task/urls.py
@@ -1,13 +1,14 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    # Examples:
-    # url(r'^$', 'fortytwo_test_task.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'', include('hello.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
